193620228da1b9c74685aaeffbc96ec8ce4d2300
Achilleas <achilleas.k@gmail.com>
2014-09-11 18:49:52 +0300

    Adding standard HH channels - Needs a K

--- basesim.py
+++ basesim.py
@@ -10,13 +10,14 @@ defaultclock.dt = dt = 0.1*ms
 duration = 300*ms
 
 length = 1000*um  # total length
-nseg = 50  # number of segments (dendrite)
+nseg = 10  # number of segments (dendrite)
 seg_length = length/nseg  # segment length
 ci = 1*uF/cm**2  # specific membrane capacitance
 gij = 0.02 * usiemens  # WAT IS THIS - PUT IT SOMEWHERE
 diam = 2*um  # diameter
 radius = diam/2  # radius
-area = pi*diam*seg_length  # segment surface area
+#area = pi*diam*seg_length  # segment surface area
+area = pi*diam*length
 Ci = ci*area  # membrane capacitance
 e_leak = -70*mV  # membrane leak potential
 rMi = 12*kohm*cm**2  # specific membrane resistance
@@ -26,8 +27,11 @@ Qi = seg_length/(pi*radius**2)  # axial resistance factor
 Rij = rL*Qi  # coupling resistance
 g_pas = 0.00004*siemens/cm**2*area  # passive channel conductance
 e_pas = -70*mV  # passive channel reversal potential
-g_Na_P = 0.12*siemens/cm**2*area  # persistent sodium conductance
-e_Na = 120*mV  # persistent sodium reversal potential
+g_Na = 35*msiemens/cm**2*area  # sodium conductance
+e_Na = 55*mV  # sodium reversal potential
+e_exc = 0*mV  # excitatory reversal potential
+tau_exc = 15*ms  # excitatory conductance time constant
+w_exc = 80*nS  # excitatory weight (conductance change)
 
 #print("Time constant =", Cm / gl)
 #print("Space constant =", .5 * (diam / (gl * Ri)) ** .5)
@@ -38,14 +42,15 @@ somaseg = nseg-1
 equations = []
 for i in range(nseg):
     equations += Equations(
-        "dV/dt = (Iin+coupling+leak+pas+I_Na_P)/Ci : volt",
+        "dV/dt = (Iin+coupling+leak+pas+Na+K+excIn)/Ci : volt",
         V="V_%i" % i,
         Iin="Iin_%i" % i,
         coupling="coupling_%i" % i,
         Ci=Ci,
         leak="leak_%i" % i,
         pas="pas_%i" % i,
-        I_Na_P="I_Na_P_%i" % i,
+        Na="Na_%i" % i,
+        excIn="excIn_%i" % i,
     )
     # leak channel
     leak_channel_eqn = "leak = (e_leak-V)/Ri : amp"
@@ -63,36 +68,60 @@ for i in range(nseg):
                            e_pas=e_pas)
     # external current
     equations += Equations("Iin : amp", Iin="Iin_%i" % i)
-    # persistent sodium (I_Na_P)
-    # TODO: FIX ME! See my_hh.py
-    persistent_sodium_channel_eqn = "I_Na_P = g_Na_P*m*(e_Na-V) : amp"
-    equations += Equations(persistent_sodium_channel_eqn,
-                           I_Na_P="I_Na_P_%i" % i,
-                           m="m_%i" % i,
+    # excitatory inputs (synapses)
+    excitatory_equation = "excIn = g_exc*(e_exc-V) : amp"
+    equations += Equations(excitatory_equation,
+                           excIn="excIn_%i" % i,
                            V="V_%i" % i,
-                           g_Na_P=g_Na_P,
-                           e_Na=e_Na)
-    equations += Equations("dm/dt = a_m*(1-m)-b_m*m : 1",
-                           dm="dm_%i" % i,
+                           g_exc="g_exc_%i" % i,
+                           e_exc=e_exc)
+    exc_conductance_equation = "dg_exc/dt = -g_exc*(1./tau_exc) : siemens"
+    equations += Equations(exc_conductance_equation,
+                           dg_exc="dg_exc_%i" % i,
+                           g_exc="g_exc_%i" % i,
+                           tau_exc=tau_exc)
+    # sodium (Na)
+    sodium_channel_eqn = "Na = g_Na*m**3*h*(e_Na-V) : amp"
+    equations += Equations(sodium_channel_eqn,
+                           Na="Na_%i" % i,
                            m="m_%i" % i,
-                           a_m="a_m_%i" % i,
-                           b_m="b_m_%i" % i)
-    equations += Equations("a_m = (2.5*mV-0.1*V)/(exp(2.5-0.1*V/mV)-1)/mV/ms : 1/second",
-                           a_m="a_m_%i" % i,
+                           h="h_%i" % i,
+                           V="V_%i" % i,
+                           g_Na=g_Na,
+                           e_Na=e_Na)
+    equations += Equations("m = alpha_m/(alpha_m+beta_m) : 1",
+                           m="m_%i" % i,
+                           alpha_m="alpha_m_%i" % i,
+                           beta_m="beta_m_%i" % i)
+    equations += Equations("alpha_m = -0.1/mV*(V+35*mV)/(exp(-0.1/mV*(V+35*mV))-1)/ms : Hz",
+                           alpha_m="alpha_m_%i" % i,
                            V="V_%i" % i)
-    equations += Equations("b_m = 4*exp(V/18.0/mV)/ms : 1/second",
-                           b_m="b_m_%i" % i,
+    equations += Equations("beta_m = 4*exp(-(V+60*mV)/(18.0*mV))/ms : Hz",
+                           beta_m="beta_m_%i" % i,
+                           V="V_%i" % i)
+    equations += Equations("dh/dt = 5*(alpha_h*(1-h)-beta_h*h) : 1",
+                           dh="dh_%i" % i,
+                           h="h_%i" % i,
+                           alpha_h="alpha_h_%i" % i,
+                           beta_h="beta_h_%i" % i)
+    equations += Equations("alpha_h = 0.07*exp(-(V+58*mV)/(20*mV))/ms : Hz",
+                           alpha_h="alpha_h_%i" % i,
+                           V="V_%i" % i)
+    equations += Equations("beta_h = 1.0/(exp(-0.1/mV*(V+28*mV))+1)/ms : Hz",
+                           beta_h="beta_h_%i" % i,
                            V="V_%i" % i)
     # coupling equations
-
     coupling_eqn = []
-    if (i > 0):
-        coupling_eqn.append("(V_pre-V_cur)/Rij")
-    if (i < nseg-1):
-        coupling_eqn.append("(V_next-V_cur)/Rij")
-    if (i == nseg-1):
-        coupling_eqn.append("(V_soma-V_cur)/Rij")
-    coupling_eqn = "coupling = "+"+".join(coupling_eqn)+" : amp"
+    #if (i > 0):
+    #    coupling_eqn.append("(V_pre-V_cur)/Rij")
+    #if (i < nseg-1):
+    #    coupling_eqn.append("(V_next-V_cur)/Rij")
+    #if (i == nseg-1):
+    #    coupling_eqn.append("(V_soma-V_cur)/Rij")
+    if coupling_eqn:
+        coupling_eqn = "coupling = "+"+".join(coupling_eqn)+" : amp"
+    else:
+        coupling_eqn = "coupling : amp"
     equations += Equations(coupling_eqn,
                            coupling="coupling_%i" % i,
                            V_pre="V_%i" % (i-1),
@@ -127,8 +156,8 @@ for i in range(nseg):
 setattr(neuron, "V_soma", e_leak)
 print("Creating input spikes ...")
 inspikes = SpikeGeneratorGroup(2, [(0, 100*ms)])
-inconn6 = Connection(inspikes[0], neuron, state="V_%i" % synlocs[1])
-inconn6.connect(inspikes, neuron, W=30*mV)
+inconn6 = Connection(inspikes[0], neuron, state="g_exc_%i" % synlocs[1])
+inconn6.connect(inspikes, neuron, W=w_exc)
 
 print("Creating monitors ...")
 trace = {}
@@ -137,7 +166,7 @@ for i in synlocs+[nseg-1]:
 trace["soma"] = StateMonitor(neuron, "V_soma", record=True)
 spikemon = SpikeMonitor(neuron)
 print("Running simulation for %s ..." % (duration))
-run(duration)
+run(duration, report="stdout")
 
 for sli in synlocs:
     print("Segment %i peaked at %f ms with %f mV" % (
@@ -149,7 +178,7 @@ figure()
 subplot(211)
 for sli in(synlocs):
     plot(trace[sli].times*1000, trace[sli][0]*1000, label="%i" % sli)
-axis([0*second, duration*1000, -80, 0])
+axis([0*second, duration*1000, -80, 40])
 ylabel("voltage (mV)")
 title("Dendrites")
 legend(loc="best")
