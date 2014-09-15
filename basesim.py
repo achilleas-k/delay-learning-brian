b13174db3e8652d080c8ba04deaf6bdc423988e2
Achilleas <achilleas.k@gmail.com>
2014-09-15 22:07:53 +0300

    Discard passive channel - add all channels to soma
    Ca channel is increasing the resting potential too much.

--- basesim.py
+++ basesim.py
@@ -6,10 +6,14 @@ from brian.library.synapses import *
 
 # TODO: Conductances per area! Add "/cm**2" to channel conductances and set
 # channel current equation units to "amp/metre**2"
+# TODO: Pay attention to differences between dendritic compartments and soma
+# TODO: Major cleanup: Split code into functions, simplify adding of channels
+# (lots of repetition in channel code), make compartmentalisation easier to
+# specify in the future without all this mess
 
 
 defaultclock.dt = dt = 0.1*ms
-duration = 3000*ms
+duration = 300*ms
 
 length = 1000*um  # total length
 nseg = 10  # number of segments (dendrite)
@@ -20,8 +24,6 @@ diam = 2*um  # diameter
 radius = diam/2  # radius
 seg_area = pi*diam*seg_length  # segment surface area
 Ci = 6*uF  #ci*area  # membrane capacitance
-g_leak = 0.3*msiemens
-e_leak = -70*mV  # membrane leak potential
 rMi = 12*kohm*cm**2  # specific membrane resistance
 rL = 200*ohm*cm  # intracellular/longitudinal resistivity
 Ri = 12*kohm  #rMi/area  # membrane resistance
@@ -30,17 +32,17 @@ Rij = rL*Qi  # coupling resistance
 Rij = Ri/10
 
 ## CHANNELS ##
-g_pas = 0.00004*siemens  #/cm**2*seg_area  # passive channel conductance
-e_pas = -70*mV  # passive channel reversal potential
+g_leak = 0.3*msiemens  # leak conductance
+e_leak = -70*mV  # membrane leak potential
 
 g_Na = 35*msiemens  #/cm**2*seg_area  # sodium conductance
-e_Na = 55*mV  # sodium reversal potential
+e_Na = 75*mV  # sodium reversal potential
 
 g_K = 9*msiemens  # potassium conductance
 e_K = -90*mV  # potassium reversal potential
 
 g_Ca = 2*msiemens  # calcium conductance
-e_Ca = 126.1*mV  # calcium reversal potential
+e_Ca = 130*mV  # calcium reversal potential
 
 e_exc = 0*mV  # excitatory reversal potential
 tau_exc = 15*ms  # excitatory conductance time constant
@@ -55,13 +57,12 @@ somaseg = nseg-1
 equations = []
 for i in range(nseg):
     equations += Equations(
-        "dV/dt = (Iin+coupling+leak+pas+Na+K+Ca+excIn)/Ci : volt",
+        "dV/dt = (Iin+coupling+leak+Na+K+Ca+excIn)/Ci : volt",
         V="V_%i" % i,
         Iin="Iin_%i" % i,
         coupling="coupling_%i" % i,
         Ci=Ci,
         leak="leak_%i" % i,
-        pas="pas_%i" % i,
         Na="Na_%i" % i,
         excIn="excIn_%i" % i,
         K="K_%i" % i,
@@ -74,13 +75,6 @@ for i in range(nseg):
                            V="V_%i" % i,
                            g_leak=g_leak,
                            e_leak=e_leak)
-    # passive channel
-    passive_channel_eqn = "pas = g_pas*(e_pas-V) : amp"
-    equations += Equations(passive_channel_eqn,
-                           pas="pas_%i" % i,
-                           V="V_%i" % i,
-                           g_pas=g_pas,
-                           e_pas=e_pas)
     # external current
     equations += Equations("Iin : amp", Iin="Iin_%i" % i)
     # excitatory inputs (synapses)
@@ -199,11 +193,14 @@ for i in range(nseg):
                            Rij=Rij)
 
 equations += Equations(
-    "dV/dt = (coupling+leak)/Ci : volt",
+    "dV/dt = (coupling+leak+Na+K+Ca)/Ci : volt",
     V="V_soma",
     coupling="coupling_soma",
     Ci=Ci,
-    leak="leak_soma"
+    leak="leak_soma",
+    Na="Na_soma",
+    K="K_soma",
+    Ca="Ca_soma"
 )
 equations += Equations("coupling = (V_pre-V_cur)/Rij : amp",
                        coupling="coupling_soma",
@@ -215,6 +212,88 @@ equations += Equations("leak = g_leak*(e_leak-V) : amp",
                        V="V_soma",
                        g_leak=g_leak,
                        e_leak=e_leak)
+# sodium (Na)
+sodium_channel_eqn = "Na = g_Na*m**3*h*(e_Na-V) : amp"
+equations += Equations(sodium_channel_eqn,
+                       Na="Na_soma",
+                       m="m_soma",
+                       h="h_soma",
+                       V="V_soma",
+                       g_Na=g_Na,
+                       e_Na=e_Na)
+sodium_state_eqns = """
+m = alpha_m/(alpha_m+beta_m) : 1
+alpha_m = -0.1/mV*(V+35*mV)/(exp(-0.1/mV*(V+35*mV))-1) : 1
+beta_m = 4*exp(-(V+60*mV)/(18.0*mV)) : 1
+
+dh/dt = (5*(alpha_h*(1-h)-beta_h*h))/ms : 1
+alpha_h = 0.07*exp(-(V+58*mV)/(20*mV)) : 1
+beta_h = 1.0/(exp(-0.1/mV*(V+28*mV))+1) : 1
+"""
+equations += Equations(sodium_state_eqns,
+                       m="m_soma",
+                       alpha_m="alpha_m_soma",
+                       beta_m="beta_m_soma",
+                       dh="dh_soma",
+                       h="h_soma",
+                       alpha_h="alpha_h_soma",
+                       beta_h="beta_h_soma",
+                       V="V_soma")
+# calcium (Ca)
+calcium_channel_eqn = "Ca = g_Ca*r**3*s*(e_Ca-V) : amp"
+equations += Equations(calcium_channel_eqn,
+                       Ca="Ca_soma",
+                       r="r_soma",
+                       s="s_soma",
+                       e_Ca=e_Ca,
+                       g_Ca=g_Ca,
+                       V="V_soma")
+calcium_state_eqns = """
+dr/dt = (alpha_r*(1-r)-beta_r*r)/ms : 1
+alpha_r = 1.0/(1.7+exp(-(V+28.2*mV)/13.5)) : 1
+beta_r = exp(-(V+63.0*mV)/7.8)/(exp(-(V+28.8*mV)/13.1)+1.7) : 1
+
+ds/dt = (alpha_s*(1-s-d)-beta_s*s)/ms : 1
+alpha_s = exp(-(V+160.3*mV)/17.8) : 1
+beta_s  = ((0.25+exp((V+83.5*mV)/6.3))**0.5-0.5) * (exp(-(V+160.3*mV)/17.8)) : 1
+
+dd/dt = (beta_d*(1-s-d)-alpha_d*d)/ms : 1
+bd     = (0.25+exp((V+83.5*mV)/6.3))**0.5 : 1
+alpha_d = (1.0+exp((V+37.4*mV)/30.0))/(240.0*(0.5+bd)) : 1
+beta_d  = (bd-0.5)*alpha_d : 1
+"""
+equations += Equations(calcium_state_eqns,
+                       V="V_soma",
+                       r="r_soma",
+                       alpha_r="alpha_r_soma",
+                       beta_r="beta_r_soma",
+                       s="s_soma",
+                       alpha_s="alpha_s_soma",
+                       beta_s="beta_s_soma",
+                       d="d_soma",
+                       alpha_d="alpha_d_soma",
+                       beta_d="beta_d_soma",
+                       bd="bd_soma"
+                       )
+# potassium (K)
+potassium_channel_eqn = "K = g_K*n**4*(e_K-V) : amp"
+equations += Equations(potassium_channel_eqn,
+                       K="K_soma",
+                       n="n_soma",
+                       e_K=e_K,
+                       g_K=g_K,
+                       V="V_soma")
+potassium_state_eqns = """
+dn/dt = 5*(alpha_n*(1-n)-beta_n*n)/ms : 1
+alpha_n = -0.01/mV*(V+34*mV)/(exp(-0.1/mV*(V+34*mV))-1) : 1
+beta_n = 0.125*exp(-(V+44*mV)/(80*mV)) : 1
+"""
+equations += Equations(potassium_state_eqns,
+                       dn="dn_soma",
+                       n="n_soma",
+                       alpha_n="alpha_n_soma",
+                       beta_n="beta_n_soma",
+                       V="V_soma")
 
 print("Setting up synapses ...")
 synlocs = [int(nseg*rel) for rel in [0.1, 0.2, 0.3, 0.6]]
